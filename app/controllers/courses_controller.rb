42341fab4d8f38d8a4428622bdbcc4fc47a5f592
DakotaLMartinez <dakotaleemusic@gmail.com>
2019-06-13 15:05:52 -0700

    add ability to respond to json to courses#index

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,11 @@
 class CoursesController < ApplicationController
   def index 
     @courses = Course.all
+    respond_to do |f|
+      f.html { render :index }
+      f.json { render json: @courses }
+    end
+    
   end
 
   def show 
