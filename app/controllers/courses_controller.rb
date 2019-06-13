8b73b17a810df7ee4ad778ed83a8ee52d02f500c
DakotaLMartinez <dakotaleemusic@gmail.com>
2019-06-13 14:13:11 -0700

    add courses index and show views

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,10 @@
 class CoursesController < ApplicationController
+  def index 
+    @courses = Course.all
+  end
+
+  def show 
+    @course = Course.find_by(id: params[:id])
+    redirect_to courses_path unless @course
+  end
 end
