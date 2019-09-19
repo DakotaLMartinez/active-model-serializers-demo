f3e4e645a0e07887326327f51276bd72689b188a
DakotaLMartinez <dakotaleemusic@gmail.com>
2019-09-19 14:12:55 -0700

    add student form

--- app/assets/javascripts/students.js
+++ app/assets/javascripts/students.js
@@ -6,7 +6,35 @@ document.addEventListener('turbolinks:load', () => {
       .then(students => displayStudents(students))
       .catch(err => displayError("Course not Found"))
   })
+
+  let newStudentForm = document.getElementById('new_student')
+  newStudentForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let data = {
+      student: {
+        name: e.target.querySelector('#student_name').value,
+        email: e.target.querySelector('#student_email').value
+      }
+    }
+    let token = e.target.querySelector('input[name=authenticity_token]').value
+    myFetch(e.target.action,{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'X-CSRF-token': token
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res=>res.json())
+      .then(studentData=> {
+        let html = new Student(studentData).render()
+        getStudentsContainer().innerHTML += html
+      })
+  })
 })
+
+const getStudentsContainer = () => document.getElementById('students')
 
 const getStudents = (courseId) => {
   return myFetch(`http://localhost:3000/courses/${courseId}/students.json`)
@@ -14,23 +42,10 @@ const getStudents = (courseId) => {
 
 const displayStudents = (results) => {
   let html = results.map(studentData => new Student(studentData).render()).join('')
-  document.getElementById('students').innerHTML = html
+  getStudentsContainer().innerHTML = html
 }
 
 const displayError = (error) => {
-  document.getElementById('students').innerHTML = error;
-}
-
-class Student {
-  constructor(attributes) {
-    this.id = attributes.id;
-    this.name = attributes.name;
-  }
-
-  render() {
-    return `
-      <div><a href="/students/${this.id}">${this.name}</a></div>
-    `
-  }
+  getStudentsContainer().innerHTML = error;
 }
 
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -32,4 +32,28 @@ class StudentsController < ApplicationController
       f.json { render json: @student }
     end
   end
+
+  def create 
+    if params[:course_id]
+      @course = Course.find_by(id: params[:course_id])
+      return unless @course # error if we don't find the course
+      @student = @course.students.build(student_params)
+      if @student.save 
+        respond_to do |f|
+          f.html { redirect_to course_path(@course) }
+          f.json { render json: @student }
+        end
+      else 
+        # some error
+      end
+    else 
+
+    end
+  end
+
+  private 
+
+  def student_params 
+    params.require(:student).permit(:name, :email)
+  end
 end
